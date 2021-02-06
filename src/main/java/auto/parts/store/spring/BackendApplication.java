71ca2042710647587ea870374d139928f88755f0
Glushkov Artem <m199956@gmail.com>
2021-02-06 18:19:32 +0300

    initial commit

---
+++ src/main/java/auto/parts/store/spring/BackendApplication.java
@@ -0,0 +1,17 @@
+package auto.parts.store.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "auto.parts.store")
+@EnableJpaRepositories(basePackages = "auto.parts.store.repository")
+@EntityScan(basePackages = "auto.parts.store.entity")
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class);
+    }
+}
